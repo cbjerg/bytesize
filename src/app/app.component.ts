2d1e2edb40e0357fc5f24556ccb1dae8ef28a53c
Christian Bjerg <cbjerg@gmail.com>
2017-03-17 10:39:07 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseObjectObservable } from 'angularfire2';
+declare var $;
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit{
+  title = 'welcome to bytesize!';
+  item: FirebaseObjectObservable<any>;
+
+  constructor(private af: AngularFire) {
+    this.item = af.database.object('/items/test');
+    console.log(this.item);
+  }
+
+  ngOnInit() {
+    $('.button-collapse').sideNav();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ProjectsComponent } from './projects/projects.component';
+
+import { AngularFireModule } from 'angularfire2';
+
+export const firebaseConfig = {
+  
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
